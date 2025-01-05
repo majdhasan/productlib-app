04527aaa7e268531c7ed35c6942d7b5c2dca8f62
Majd Hasan <majd.hasan@bettermile.com>
2025-01-05 12:25:38 +0100

    update confirmation page

--- src/pages/Confirmation/Confirmation.tsx
+++ src/pages/Confirmation/Confirmation.tsx
@@ -1,33 +1,15 @@
-import React, { useEffect, useState } from "react";
-import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonButton,
-  IonSpinner,
-  IonButtons,
-  IonBackButton,
-  IonList,
-  IonItem,
-  IonLabel,
-  IonThumbnail
-} from "@ionic/react";
-import { useParams, useHistory } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonText, IonThumbnail, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/react';
 import { OrderAPI } from "../../services/apiService";
 import { useAppContext } from "../../context/AppContext";
 import { translations } from '../../translations';
 import "./Confirmation.css";
 
-// TODO rework this
-
 const Confirmation: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const { language, isLoading, setIsLoading } = useAppContext();
   const labels = translations[language];
-
-  const history = useHistory();
   const [order, setOrder] = useState<any>(null);
 
   useEffect(() => {
@@ -44,18 +26,18 @@ const Confirmation: React.FC = () => {
     };
 
     fetchOrderDetails();
-  }, [id]);
+  }, [id, setIsLoading]);
 
   if (isLoading) {
     return (
       <IonPage>
         <IonHeader>
           <IonToolbar>
-            <IonTitle>{labels.loading}</IonTitle>
+            <IonTitle>{labels.orderConfirmation}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent>
-          <IonSpinner />
+        <IonContent className="ion-padding">
+          <IonText>{labels.loading}</IonText>
         </IonContent>
       </IonPage>
     );
@@ -66,79 +48,131 @@ const Confirmation: React.FC = () => {
       <IonPage>
         <IonHeader>
           <IonToolbar>
-            <IonButtons slot="start">
-              <IonBackButton defaultHref="/" />
-            </IonButtons>
-            <IonTitle>{labels.orderNotFound}</IonTitle>
+            <IonTitle>{labels.orderConfirmation}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent>
-          <p>{labels.orderRetrievalFailed}</p>
+        <IonContent className="ion-padding">
+          <IonText>{labels.orderNotFound}</IonText>
         </IonContent>
       </IonPage>
     );
   }
 
-  const statusMessage = order.isPaid
-    ? labels.orderConfirmed
-    : labels.orderAwaitingPayment;
+  const calculateTotalCost = () => {
+    return order.cart.items.reduce((total: number, item: any) => total + (item.productPrice * item.quantity), 0).toFixed(2);
+  };
 
-  const calculateRowTotal = (quantity: number, price: number) =>
-    quantity * price;
+  const getStatusLabel = (status: string) => {
+    switch (status) {
+      case 'SUBMITTED':
+        return labels.statusSUBMITTED;
+      case 'APPROVED':
+        return labels.statusAPPROVED;
+      case 'READY_FOR_PICKUP':
+        return labels.statusREADY_FOR_PICKUP;
+      case 'IN_DELIVERY':
+        return labels.statusIN_DELIVERY;
+      case 'DELIVERED':
+        return labels.statusDELIVERED;
+      case 'FAILED':
+        return labels.statusFAILED;
+      default:
+        return labels.unknownStatus;
+    }
+  };
+
+  const getTypeLabel = (type: string) => {
+    switch (type) {
+      case 'PICKUP':
+        return labels.pickup;
+      case 'DELIVERY':
+        return labels.delivery;
+      default:
+        return labels.unknownStatus;
+    }
+  };
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonButtons slot="start">
-            <IonBackButton defaultHref="/" />
-          </IonButtons>
-          <IonTitle>{labels.orderDetails}</IonTitle>
+          <IonTitle>{labels.orderConfirmation}</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        <h2>{statusMessage}</h2>
-        <p>
-          <strong>{labels.orderId}:</strong> {order.id}
-        </p>
-        <p>
-          <span className={order.isPaid ? "paid" : "unpaid"}>
-            {order.isPaid ? labels.statusPaid : labels.statusUnpaid}
-          </span>
-        </p>
-
-        <IonList>
-          {order.cart.items.map((item: any) => (
-            <IonItem key={item.id}>
-              <IonThumbnail slot="start">
-                <img
-                  // src={`http://localhost:8080/images/${item.product.image}`}
-                  src={`https://pbs.twimg.com/media/Dq_Dic9W4AAQo9c.png`}
-                  alt={item.product.name}
-                />
-              </IonThumbnail>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>{labels.orderDetails}</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <IonList>
+            <IonItem>
+                <IonLabel>{labels.orderStatus}</IonLabel>
+                <IonText>{getStatusLabel(order.status)}</IonText>
+              </IonItem>
+              <IonItem>
+                <IonLabel>{labels.orderType}</IonLabel>
+                <IonText>{getTypeLabel(order.type)}</IonText>
+              </IonItem>
+              <IonItem>
+                <IonLabel>{labels.wishedPickupTime}</IonLabel>
+                <IonText>{order.wishedPickupTime || labels.asSoonAsPossible}</IonText>
+              </IonItem>
+              <IonItem>
+                <IonLabel>{labels.firstName}</IonLabel>
+                <IonText>{order.firstName}</IonText>
+              </IonItem>
+              <IonItem>
+                <IonLabel>{labels.lastName}</IonLabel>
+                <IonText>{order.lastName}</IonText>
+              </IonItem>
+              <IonItem>
+                <IonLabel>{labels.phoneNumber}</IonLabel>
+                <IonText>{order.phone}</IonText>
+              </IonItem>
+              <IonItem>
+                <IonLabel>{labels.orderAddress}</IonLabel>
+                <IonText>{order.address}</IonText>
+              </IonItem>
+            </IonList>
+          </IonCardContent>
+        </IonCard>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>{labels.products}</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <IonList>
+              {order.cart.items.map((item: any, index: number) => (
+                <IonItem key={index}>
+                  <IonThumbnail slot="start">
+                    <img
+                      src={`https://pbs.twimg.com/media/Dq_Dic9W4AAQo9c.png`}
+                      alt={item.product.name}
+                    />
+                  </IonThumbnail>
+                  <IonLabel>
+                    <h2>{item.product.name}</h2>
+                    <p>{labels.quantity}: {item.quantity}</p>
+                    <p>{labels.price}: ₪{item.productPrice}</p>
+                    <p>
+                      <strong>{labels.rowTotal}:</strong> ₪
+                      {(item.productPrice * item.quantity).toFixed(2)}
+                    </p>
+                  </IonLabel>
+                </IonItem>
+              ))}
+            </IonList>
+            <IonItem>
               <IonLabel>
-                <h2>{item.product.name}</h2>
-                <p>
-                  <strong>{labels.quantity}:</strong> {item.quantity}
-                </p>
-                <p>
-                  <strong>{labels.pricePerUnit}:</strong> ₪{item.productPrice.toFixed(2)}
-                </p>
-                <p>
-                  <strong>{labels.total}:</strong> ₪{calculateRowTotal(item.quantity, item.productPrice).toFixed(2)}
-                </p>
-                {item.notes && <p><strong>{labels.notes}:</strong> {item.notes}</p>}
+                <h2>{labels.total}</h2>
               </IonLabel>
+              <IonText>
+                <h2>₪{calculateTotalCost()}</h2>
+              </IonText>
             </IonItem>
-          ))}
-        </IonList>
-
-        {!order.isPaid && (
-          <IonButton expand="block" color="success" onClick={() => history.push(`/payment/${id}`)}>
-            {labels.payNow}
-          </IonButton>
-        )}
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
